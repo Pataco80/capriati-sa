2eba9dab07a0a11d26a79de51e1bc02fd4d8b349
Ricardo Do Vale <pataco80@gmail.com>
2021-10-10 22:27:58 +0200

    Change links from after merge conflicts

--- src/components/constants/socialLinks.js
+++ src/components/constants/socialLinks.js
@@ -1,22 +1,22 @@
-import React from 'react'
+import React from "react"
 import {
   FacebookWithCircle,
   TwitterWithCircle,
   LinkedinWithCircle,
-} from '@styled-icons/entypo-social'
+} from "@styled-icons/entypo-social"
 
 const socialLinks = [
   {
     icon: <FacebookWithCircle />,
-    url: 'www.facebook.com',
+    url: "www.facebook.com",
   },
   {
     icon: <TwitterWithCircle />,
-    url: 'www.twitter.com',
+    url: "www.twitter.com",
   },
   {
     icon: <LinkedinWithCircle />,
-    url: 'www.linkedin.com',
+    url: "www.linkedin.com",
   },
 ]
 
