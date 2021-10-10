df5f6489febb8a73e3b4c0c84465d97e0a8f1a08
Ricardo Do Vale <pataco80@gmail.com>
2021-10-10 15:40:00 +0200

    Add configuration site config to gatsby-config.js
    Add variables for siteMetadata, plugin manifest and offline

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,50 @@
+// Initialize dotenv
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`, // or '.env'
+})
+
+const websiteConfig = require('./config/website-config')
+// And then you can use the config in gatsby-config.js
+const pathPrefix = websiteConfig.pathPrefix
+const siteUrl = websiteConfig.siteUrl + pathPrefix
+
 module.exports = {
+  /* General Information */
+  pathPrefix: websiteConfig.pathPrefix,
   siteMetadata: {
-    siteUrl: 'https://www.yourdomain.tld',
-    title: 'capriati-sa',
+    siteUrl: siteUrl, // For gatsby-plugin-sitemap
+    pathPrefix,
+    siteTitle: websiteConfig.siteTitle,
+    siteDescription: websiteConfig.siteDescription,
+    logo: websiteConfig.siteLogo,
+    siteLanguage: websiteConfig.siteLanguage,
+    siteKeywords: websiteConfig.siteKeywords,
+    bossName: websiteConfig.bossFullName,
+    bossPosition: websiteConfig.bossPosition,
+    bossDescription: websiteConfig.bossDescription,
+    bossSlogan: websiteConfig.bossSlogan,
+    businessRoad: websiteConfig.businessAdressRoad,
+    businessRoadNumber: websiteConfig.businessAdressRoadNumber,
+    businessZipCode: websiteConfig.businessAdressZipCode,
+    businessCity: websiteConfig.businessAdressCity,
+    businessCanton: websiteConfig.businessAdressCanton,
+    businessShortCanton: websiteConfig.businessAdressShortCanton,
+    businessContry: websiteConfig.businessAdressCountry,
+    businessShortCountry: websiteConfig.businessAdressShortCountry,
+    businessPhone: websiteConfig.businessPhone,
+    businessFax: websiteConfig.businessFax,
+    businessEmail: websiteConfig.businessEmail,
+    copyright: websiteConfig.copyright,
+    twitterUserName: websiteConfig.twitterUserName,
+    socialMenu: websiteConfig.socialLinks,
+    facebookUserName: websiteConfig.facebookUserName,
+    conceptorWebSite: websiteConfig.webSiteConceptor,
+    formSpree: websiteConfig.formSpree,
   },
   plugins: [
     'gatsby-plugin-styled-components',
     'gatsby-plugin-gatsby-cloud',
     'gatsby-plugin-image',
-    'gatsby-plugin-react-helmet',
     'gatsby-plugin-sitemap',
     {
       resolve: 'gatsby-plugin-manifest',
@@ -33,5 +70,56 @@ module.exports = {
       },
       __key: 'assets',
     },
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: websiteConfig.siteTitle,
+        short_name: websiteConfig.siteTitleShort,
+        description: websiteConfig.siteDescription,
+        start_url: '/',
+        background_color: websiteConfig.backgroundColor,
+        theme_color: websiteConfig.themeColor,
+        display: 'standalone',
+        icon: websiteConfig.favicon,
+        icons: [
+          {
+            src: '/favicons/android-icon-36x36.png',
+            sizes: '36x36',
+            type: 'image/png',
+          },
+          {
+            src: '/favicons/android-icon-48x48.png',
+            sizes: '48x48',
+            type: 'image/png',
+          },
+          {
+            src: '/favicons/android-icon-72x72.png',
+            sizes: '72x72',
+            type: 'image/png',
+          },
+          {
+            src: '/favicons/android-icon-96x96.png',
+            sizes: '96x96',
+            type: 'image/png',
+          },
+          {
+            src: '/favicons/android-icon-144x144.png',
+            sizes: '144x144',
+            type: 'image/png',
+          },
+          {
+            src: '/favicons/favicon.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+        icon_options: {
+          purpose: `maskable`,
+        },
+        crossOrigin: `use-credentials`,
+        cache_busting_mode: 'none',
+      },
+    },
+    `gatsby-plugin-offline`,
   ],
 }
