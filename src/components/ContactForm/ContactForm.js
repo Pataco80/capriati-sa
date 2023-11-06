7c7e0aa99bf4d0212752316be1067ab4b79eacc5
Ricardo Do Vale <pataco80@gmail.com>
2023-11-06 16:54:35 +0100

    test formulaire

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 
 // Import Components for App
 import { Title } from "@components"
@@ -12,81 +12,122 @@ import * as S from "./ContactFormStyled"
 
 // Component
 const Contactform = ({ formID, formCaptcha, className }) => {
-  // Render Component
-  return (
-    <Section>
-      <Title tag='h3' title='Contactez-nous via notre formulaire' />
-      <S.FormContainer padding='0'>
-        <S.ContactForm
-          action='https://formspree.io/f/xzbkneqk'
-          method='POST'
-          autocomplete='on'
-        >
-          <S.ContactInfo>
-            <input type='text' name='_gotcha' style={{ display: "none" }} />
-            <S.InputWrapper>
-              <S.Label htmlFor='name'>Nom</S.Label>
-              <S.Input
-                type='text'
-                name='name'
-                id='name'
-                placeholder='Saisissez votre Nom'
-                required
-              />
-            </S.InputWrapper>
-            <S.InputWrapper>
-              <S.Label htmlFor='email'>Email</S.Label>
-              <S.Input
-                type='email'
-                name='_replyto'
-                id='email'
-                placeholder='Saisissez votre adresse mail'
-                required
-              />
-            </S.InputWrapper>
-            <S.InputWrapper>
-              <S.Label htmlFor='phone'>Tel. Fixe</S.Label>
-              <S.Input
-                type='tel'
-                name='phone'
-                id='phone'
-                placeholder='Votre N° fixe (facultatif)'
-              />
-            </S.InputWrapper>
-            <S.InputWrapper>
-              <S.Label htmlFor='mobile'>Tel. Mobile</S.Label>
-              <S.Input
-                type='tel'
-                name='mobile'
-                id='mobile'
-                placeholder='Votre N° mobile (facultatif)'
-              />
-            </S.InputWrapper>
-          </S.ContactInfo>
-          <S.ContactMessage>
-            <S.Label htmlFor='message'>Message</S.Label>
-            <S.TextArea
-              name='message'
-              id='message'
-              placeholder='Saisissez votre Message'
-              rows='10'
-              required
-            />
-          </S.ContactMessage>
-          <S.Captcha
-            className={`g-recaptcha ${className}`}
-            data-sitekey={formCaptcha}
-          ></S.Captcha>
-          <ButtonForm
-            primary
-            type='submit'
-            value='Envoyer'
-            title='Envoyer le formulaire'
-          />
-        </S.ContactForm>
-      </S.FormContainer>
-    </Section>
-  )
+	const [subjectMail, setSubjectMail] = useState(null)
+
+	const onOptionChange = (e) => {
+		setSubjectMail(e.target.value)
+	}
+	// Render Component
+	return (
+		<Section>
+			<S.FormContainer padding='0'>
+				<S.ContactForm
+					action='https://formspree.io/f/xzbkneqk'
+					method='POST'
+					autocomplete='on'>
+					<S.ContactInfo>
+						<input type='text' name='_gotcha' style={{ display: "none" }} />
+						<S.InputWrapper>
+							<S.Label htmlFor='name'>Nom</S.Label>
+							<S.Input
+								type='text'
+								name='name'
+								id='name'
+								placeholder='Saisissez votre Nom'
+								required
+							/>
+						</S.InputWrapper>
+						<S.InputWrapper>
+							<S.Label htmlFor='email'>Email</S.Label>
+							<S.Input
+								type='email'
+								name='_replyto'
+								id='email'
+								placeholder='Saisissez votre adresse mail'
+								required
+							/>
+						</S.InputWrapper>
+						<S.InputWrapper>
+							<S.Label htmlFor='phone'>Tel. Fixe</S.Label>
+							<S.Input
+								type='tel'
+								name='phone'
+								id='phone'
+								placeholder='Votre N° fixe (facultatif)'
+							/>
+						</S.InputWrapper>
+						<S.InputWrapper>
+							<S.Label htmlFor='mobile'>Tel. Mobile</S.Label>
+							<S.Input
+								type='tel'
+								name='mobile'
+								id='mobile'
+								placeholder='Votre N° mobile (facultatif)'
+							/>
+						</S.InputWrapper>
+						<Title tag='h6' title='Raison de votre contact' />
+
+						<S.InputWrapper className='radio-input'>
+							<S.RadioInput
+								className='first'
+								type='radio'
+								name='topping'
+								value='Regular'
+								id='regular'
+								checked={subjectMail === "Regular"}
+								onChange={onOptionChange}
+								required
+							/>
+							<S.Label className='radio-input-label' htmlFor='regular'>
+								Regular
+							</S.Label>
+							<S.RadioInput
+								type='radio'
+								name='topping'
+								value='Medium'
+								id='medium'
+								checked={subjectMail === "Medium"}
+								onChange={onOptionChange}
+							/>
+							<S.Label className='radio-input-label' htmlFor='medium'>
+								Medium
+							</S.Label>
+							<S.RadioInput
+								type='radio'
+								name='topping'
+								value='Large'
+								id='large'
+								checked={subjectMail === "Large"}
+								onChange={onOptionChange}
+							/>
+							<S.Label className='radio-input-label' htmlFor='large'>
+								Large
+							</S.Label>
+						</S.InputWrapper>
+					</S.ContactInfo>
+					<S.ContactMessage>
+						<S.Label htmlFor='message'>Message</S.Label>
+						<S.TextArea
+							name='message'
+							id='message'
+							placeholder='Saisissez votre Message'
+							rows='10'
+							required
+						/>
+					</S.ContactMessage>
+					<S.Captcha
+						className={`g-recaptcha ${className}`}
+						data-sitekey={formCaptcha}></S.Captcha>
+					<ButtonForm
+						primary
+						type='submit'
+						value='Envoyer'
+						title='Envoyer le formulaire'
+					/>
+				</S.ContactForm>
+			</S.FormContainer>
+		</Section>
+	)
 }
 
 export default Contactform
--- src/components/ContactForm/ContactFormStyled.js
+++ src/components/ContactForm/ContactFormStyled.js
@@ -5,86 +5,110 @@ import { SectionContent } from "../StyledElements/SectionStyled"
 
 // Import helpers
 import {
-  setFlex,
-  layout,
-  media,
-  setRadius,
-  setBorder,
-  setColor,
-  setShadow,
-  setFont,
+	setFlex,
+	layout,
+	media,
+	setRadius,
+	setBorder,
+	setColor,
+	setShadow,
+	setFont,
+	spaceScale,
 } from "@helpers"
 
 // Styled CSS
 const ContactInput = css`
-  font-family: ${setFont.bodyFont};
-  font-size: 1rem;
-  width: 100%;
-  padding: 1.5rem;
-  margin: 1rem 0 3rem;
-  ${setRadius({ allPx: 12 })};
-  border: ${setBorder({
-    size: 1,
-    style: "solid",
-    color: setColor.primaryColor,
-  })};
-  background: ${setColor.mainGreyL3};
-  box-shadow: ${setShadow("light")};
+	font-family: ${setFont.bodyFont};
+	font-size: 1rem;
+	width: 100%;
+	padding: 1.5rem;
+	margin: 1rem 0 3rem;
+	${setRadius({ allPx: 12 })};
+	border: ${setBorder({
+		size: 1,
+		style: "solid",
+		color: setColor.primaryColor,
+	})};
+	background: ${setColor.mainGreyL3};
+	box-shadow: ${setShadow("light")};
 
-  &:focus {
-    box-shadow: ${setShadow("light", { inset: "inset" })};
-    outline: none;
-  }
-  &::placeholder {
-    font-style: italic;
-  }
+	&:focus {
+		box-shadow: ${setShadow("light", { inset: "inset" })};
+		outline: none;
+	}
+	&::placeholder {
+		font-style: italic;
+	}
 `
 
 // Export Styles for the component
 export const FormContainer = styled(SectionContent)`
-  ${layout({ mW: 800 })};
-  ${setFlex({ flDir: "column" })};
+	${layout({ mW: 800 })};
+	${setFlex({ flDir: "column" })};
 `
 
 export const ContactForm = styled.form`
-  ${setFlex({ flDir: "column" })};
-  width: 100%;
+	${setFlex({ flDir: "column" })};
+	width: 100%;
 `
 
 export const ContactInfo = styled.section`
-  width: 100%;
+	width: 100%;
 
-  ${media.greaterThan("smTablet")`
+	${media.greaterThan("smTablet")`
     ${setFlex({ x: "space-between" })};
   `}
+
+	h6 {
+		width: 100%;
+		margin-bottom: ${spaceScale.space2xs};
+	}
 `
 
 export const ContactMessage = styled.section`
-  width: 100%;
+	width: 100%;
+	margin-top: ${spaceScale.spaceM};
 `
 
 export const InputWrapper = styled.div`
-  width: 100%;
+	width: 100%;
 
-  ${media.greaterThan("smTablet")`
+	${media.greaterThan("smTablet")`
     width: 45%;
+
+		&.radio-input {
+			    width: 100%;
+
+		}
   `}
 `
 
 export const Label = styled.label`
-  font-family: ${setFont.headingsFont};
-  color: ${setColor.primaryColor};
+	font-family: ${setFont.headingsFont};
+	color: ${setColor.primaryColor};
+
+	&.radio-input-label {
+		margin-left: 0.25rem;
+	}
 `
 
 export const Input = styled.input`
-  ${ContactInput}
+	${ContactInput}
 `
 
 export const TextArea = styled.textarea`
-  ${ContactInput}
-  resize: none;
+	${ContactInput}
+	resize: none;
 `
 
 export const Captcha = styled.div`
-  margin-bottom: 3rem;
+	margin-bottom: 3rem;
+`
+
+export const RadioInput = styled.input`
+	margin-left: 1rem;
+
+	&.first {
+		margin-left: 0;
+	}
 `
